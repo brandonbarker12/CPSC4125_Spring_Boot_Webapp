5021c1dacbb1c99d3ef232936ee6d9ebec623097
brandonbarker12 <brandonjbarker12@gmail.com>
2020-09-15 22:39:42 +0000

    Modifying hello controller

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -24,9 +24,9 @@ public class HelloController {
     @PostMapping("/contact")
     public String postContact(Model model, @RequestBody Contact contact){
         //Contact create object
-        System.out.println(contact.getEmail());
-        System.out.println(contact.getMessage());
-        return "contact";
+        model.addAttribute("email", contact.getEmail());
+        model.addAttribute("message", contact.getMessage());
+        return "fragments/contactResponse :: #contactSuccess";
     }
 
     @GetMapping("/about")
